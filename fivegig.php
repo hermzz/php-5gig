ee43383b3b876335a9c713265d2c62ff121303c6
Hermann Kaser <hermzz@woodstock.(none)>
2010-04-27 16:16:13 +0200

    Add rest of API methods

--- fivegig.php
+++ fivegig.php
@@ -15,11 +15,36 @@ class FiveGig
 		return $this->_request('user.getEvents', array('user' => $username, 'past' => $past));
 	}
 	
+	public function venueGet($venue_id)
+	{
+		return $this->_request('venue.get', array('venue_id' => $venue_id));
+	}
+	
+	public function venueGetEvents($venue_id, $past=false)
+	{
+		return $this->_request('venue.getEvents', array('venue_id' => $venue_id, 'past' => $past));
+	}
+	
+	public function cityGetEvents($city, $country_iso=false, $past=false)
+	{
+		return $this->_request('city.getEvents', array('city' => $city, 'country_iso' => $country_iso, 'past' => $past));
+	}
+	
+	public function artistGetEvents($artist, $country_iso=false, $past=false)
+	{
+		return $this->_request('city.getEvents', array('artist' => $artist, 'country_iso' => $country_iso, 'past' => $past));
+	}
+	
+	public function setDomain($domain)
+	{
+		$this->_domain = $domain;
+	}
+	
 	private function _request($method, $params)
 	{
 		$tmp_params = array();
 		foreach($params as $k => $v)
-			$tmp_params[] = $k.'='.$v;
+			$tmp_params[] = $k.'='.urlencode($v);
 			
 		$url = sprintf('http://www.%s/api/request.php?api_key=%s&method=%s&%s&format=json',
 			$this->_domain, $this->_api_key, $method, implode('&', $tmp_params));
